752cfe72ae2f6873ff0652f957b3789a80310e56
koyumi <koyumi0601@gmail.com>
2023-12-21 20:13:59 +0900

    tabby cat

--- _posts/MachineLearning/Lecture_Legend13_Record/CAM_ClassActivationMap/cam.py
+++ _posts/MachineLearning/Lecture_Legend13_Record/CAM_ClassActivationMap/cam.py
@@ -9,7 +9,7 @@ import numpy as np
 import cv2
 
 ## 이미지 경로 설정
-img_path = r'D:\GitHub_Project\koyumi0601.github.io\_posts\MachineLearning\Lecture_Legend13_Record\CAM\cat.jpg'  # 분석할 이미지 파일 경로
+img_path = r'D:\GitHub_Project\koyumi0601.github.io\_posts\MachineLearning\Lecture_Legend13_Record\CAM_ClassActivationMap\cat.jpg'  # 분석할 이미지 파일 경로
 
 ## Resnet은 ImageNet에서 Training 되었으므로 image Net의 class 정보를 가져옵니다.
 classes_url = 'https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt'
