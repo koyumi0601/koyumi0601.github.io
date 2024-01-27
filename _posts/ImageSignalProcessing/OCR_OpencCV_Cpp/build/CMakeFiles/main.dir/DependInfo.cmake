44e47cdc29ae66ccb8bb3412f34ac84dc3c02675
koyumi0601@gmail.com <koyumi0601@gmail.com>
2024-01-27 14:21:41 +0900

    backup

---
+++ _posts/ImageSignalProcessing/OCR_OpencCV_Cpp/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ko/Documents/GitHub/koyumi0601.github.io/_posts/ImageSignalProcessing/OCR_OpencCV_Cpp/basic_ocr.cpp" "CMakeFiles/main.dir/basic_ocr.cpp.o" "gcc" "CMakeFiles/main.dir/basic_ocr.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
