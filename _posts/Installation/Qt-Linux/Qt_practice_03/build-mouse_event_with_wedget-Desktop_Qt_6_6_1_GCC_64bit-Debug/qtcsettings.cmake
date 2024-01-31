06af4650d6b3f24bfbfd20b9035b27b4e4ed9f47
koyumi0601@gmail.com <koyumi0601@gmail.com>
2024-01-31 11:06:45 +0900

    back

---
+++ _posts/Installation/Qt-Linux/Qt_practice_03/build-mouse_event_with_wedget-Desktop_Qt_6_6_1_GCC_64bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/ko/Documents/GitHub/koyumi0601.github.io/_posts/Installation/Qt-Linux/Qt_practice_03/build-mouse_event_with_wedget-Desktop_Qt_6_6_1_GCC_64bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/ko/Qt/6.6.1/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/ko/Qt/6.6.1/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/x86_64-linux-gnu-gcc" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
