8856891406b7cfe37051c9949783f08a0a83c087
koyumi0601 <120715479+koyumi0601@users.noreply.github.com>
2024-02-19 10:38:44 +0900

    delete duplicated project

--- _posts/Cuda/practice_029_window_cuda/CudaRuntime1/CudaRuntime1/kernel.cuh
+++
@@ -1,8 +0,0 @@
-#ifndef KERNEL_CUH_
-#define KERNEL_CUH_
-
-#include <cuda_runtime.h>
-
-void calculateAverage(int N, int M, int L, dim3 numBlocks, dim3 blockSize, float *inputData, float *outputData);
-
-#endif // KERNEL_CUH_
